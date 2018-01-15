0f3d7d0d9f55bc50c8e75b0434ab8afac3d14bc0
gilney nathanael mathias <gilney_salvo@hotmail.com>
2018-01-15 11:55:59 -0200

    Adicionar StatusBar

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import { Provider } from "react-redux";
+import { StatusBar, View, StyleSheet } from "react-native";
 
+import { colors } from "./constants/styles";
 import store from "./store";
 import App from "./App";
 
@@ -8,10 +10,22 @@ class Main extends Component {
   render() {
     return (
       <Provider store={store}>
-        <App />
+        <View style={styles.container}>
+          <StatusBar
+            backgroundColor={colors.primary_dark}
+            barStyle="light-content"
+          />
+          <App />
+        </View>
       </Provider>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  }
+});
 
 export default Main;
